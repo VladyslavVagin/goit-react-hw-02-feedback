691df57879a74a69c478ce2d45c1754c591ba1c5
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-11-24 21:46:13 +0100

    update
    Step_2 ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,37 +8,57 @@ export class App extends Component {
     bad: 0,
   };
 
-  handleClickGood = () => this.setState(prevStateGood => ({good: prevStateGood.good + 1}))
-  handleClickNeutral = () => this.setState(prevStateNeutral => ({neutral: prevStateNeutral.neutral + 1}))
-  handleClickBad = () => this.setState(prevStateBad => ({bad: prevStateBad.bad + 1}))
+  handleClickGood = () =>
+    this.setState(prevStateGood => ({ good: prevStateGood.good + 1 }));
+  handleClickNeutral = () =>
+    this.setState(prevStateNeutral => ({
+      neutral: prevStateNeutral.neutral + 1,
+    }));
+  handleClickBad = () =>
+    this.setState(prevStateBad => ({ bad: prevStateBad.bad + 1 }));
 
   countTotalFeedback = () => {
     let summ = 0;
-    const arrayOfFeedbacks = Object.values(this.state); 
+    const arrayOfFeedbacks = Object.values(this.state);
     for (let i = 0; i < arrayOfFeedbacks.length; i++) {
       summ += arrayOfFeedbacks[i];
     }
     return summ;
-  }
-
+  };
+  countPositiveFeedbackPercentage = total => {
+    const positivePercent = (this.state.good / total) * 100 || 0;
+    return Math.round(positivePercent);
+  };
 
   render() {
     return (
       <div>
         <h1>Please, leave feedback</h1>
-        <ul className={css.buttonList}> 
+        <ul className={css.buttonList}>
           <li>
-            <button type="button" className={css.goodBtn} onClick={this.handleClickGood}>
+            <button
+              type="button"
+              className={css.goodBtn}
+              onClick={this.handleClickGood}
+            >
               Good
             </button>
           </li>
           <li>
-            <button type="button" className={css.neutralBtn} onClick={this.handleClickNeutral}>
+            <button
+              type="button"
+              className={css.neutralBtn}
+              onClick={this.handleClickNeutral}
+            >
               Neutral
             </button>
           </li>
           <li>
-            <button type="button" className={css.badBtn} onClick={this.handleClickBad}>
+            <button
+              type="button"
+              className={css.badBtn}
+              onClick={this.handleClickBad}
+            >
               Bad
             </button>
           </li>
@@ -46,19 +66,35 @@ export class App extends Component {
 
         <ul>
           <li>
-            <p className={css.statisticText}>Good<span>{this.state.good}</span></p>
+            <p className={css.statisticText}>
+              Good<span>{this.state.good}</span>
+            </p>
           </li>
           <li>
-            <p className={css.statisticText}>Neutral<span>{this.state.neutral}</span></p>
+            <p className={css.statisticText}>
+              Neutral<span>{this.state.neutral}</span>
+            </p>
           </li>
           <li>
-            <p className={css.statisticText}>Bad<span>{this.state.bad}</span></p>
+            <p className={css.statisticText}>
+              Bad<span>{this.state.bad}</span>
+            </p>
           </li>
           <li>
-            <p className={css.statisticText}>Total<span>{this.countTotalFeedback()}</span></p>
+            <p className={css.statisticText}>
+              Total<span>{this.countTotalFeedback()}</span>
+            </p>
           </li>
           <li>
-            <p className={css.statisticText} onClick={this.countTotalFeedback}>Positive feedback<span></span></p>
+            <p className={css.statisticText}>
+              Positive feedback
+              <span>
+                {this.countPositiveFeedbackPercentage(
+                  this.countTotalFeedback()
+                )}
+                %
+              </span>
+            </p>
           </li>
         </ul>
       </div>
