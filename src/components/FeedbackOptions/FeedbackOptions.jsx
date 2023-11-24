8b83cf85f754ca5e8f503a1f6a0451c050b8068f
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-11-24 22:57:11 +0100

    update

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,40 +1,47 @@
-import React from 'react'
-import css from './FeedbackOptions.module.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './FeedbackOptions.module.css';
 
-const FeedbackOptions = ({handleClickGood, handleClickNeutral, handleClickBad}) => {
+const FeedbackOptions = ({
+  handleClickGood,
+  handleClickNeutral,
+  handleClickBad,
+}) => {
   return (
     <div>
-         <ul className={css.buttonList}>
-          <li>
-            <button
-              type="button"
-              className={css.goodBtn}
-              onClick={handleClickGood}
-            >
-              Good
-            </button>
-          </li>
-          <li>
-            <button
-              type="button"
-              className={css.neutralBtn}
-              onClick={handleClickNeutral}
-            >
-              Neutral
-            </button>
-          </li>
-          <li>
-            <button
-              type="button"
-              className={css.badBtn}
-              onClick={handleClickBad}
-            >
-              Bad
-            </button>
-          </li>
-        </ul>
+      <ul className={css.buttonList}>
+        <li>
+          <button
+            type="button"
+            className={css.goodBtn}
+            onClick={handleClickGood}
+          >
+            Good
+          </button>
+        </li>
+        <li>
+          <button
+            type="button"
+            className={css.neutralBtn}
+            onClick={handleClickNeutral}
+          >
+            Neutral
+          </button>
+        </li>
+        <li>
+          <button type="button" className={css.badBtn} onClick={handleClickBad}>
+            Bad
+          </button>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default FeedbackOptions
+FeedbackOptions.propTypes = {
+  handleClickGood: PropTypes.func.isRequired,
+  handleClickNeutral: PropTypes.func.isRequired,
+  handleClickBad: PropTypes.func.isRequired,
+};
+
+export default FeedbackOptions;
