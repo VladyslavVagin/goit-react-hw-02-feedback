2e0f5561f3f77b3ed066086a22a190cdfb0a38d7
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-11-24 21:28:04 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import css from './App.module.css';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleClickGood = () => this.setState(prevStateGood => ({good: prevStateGood.good + 1}))
+  handleClickNeutral = () => this.setState(prevStateNeutral => ({neutral: prevStateNeutral.neutral + 1}))
+  handleClickBad = () => this.setState(prevStateBad => ({bad: prevStateBad.bad + 1}))
+
+  countTotalFeedback = () => {
+    let summ = 0;
+    const arrayOfFeedbacks = Object.values(this.state); 
+    for (let i = 0; i < arrayOfFeedbacks.length; i++) {
+      summ += arrayOfFeedbacks[i];
+    }
+    return summ;
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Please, leave feedback</h1>
+        <ul className={css.buttonList}> 
+          <li>
+            <button type="button" className={css.goodBtn} onClick={this.handleClickGood}>
+              Good
+            </button>
+          </li>
+          <li>
+            <button type="button" className={css.neutralBtn} onClick={this.handleClickNeutral}>
+              Neutral
+            </button>
+          </li>
+          <li>
+            <button type="button" className={css.badBtn} onClick={this.handleClickBad}>
+              Bad
+            </button>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <p className={css.statisticText}>Good<span>{this.state.good}</span></p>
+          </li>
+          <li>
+            <p className={css.statisticText}>Neutral<span>{this.state.neutral}</span></p>
+          </li>
+          <li>
+            <p className={css.statisticText}>Bad<span>{this.state.bad}</span></p>
+          </li>
+          <li>
+            <p className={css.statisticText}>Total<span>{this.countTotalFeedback()}</span></p>
+          </li>
+          <li>
+            <p className={css.statisticText} onClick={this.countTotalFeedback}>Positive feedback<span></span></p>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
