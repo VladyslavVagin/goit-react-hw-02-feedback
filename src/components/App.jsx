32bc69b17c252d839e7802dbf010640b16ed2a0f
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-11-24 21:52:18 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,15 +8,21 @@ export class App extends Component {
     bad: 0,
   };
 
+//================================ Increse Good Feedback function
   handleClickGood = () =>
     this.setState(prevStateGood => ({ good: prevStateGood.good + 1 }));
+
+//================================= Increase Neutral Feedback function
   handleClickNeutral = () =>
     this.setState(prevStateNeutral => ({
       neutral: prevStateNeutral.neutral + 1,
     }));
+
+//==================================== Increase Bad Feedback function
   handleClickBad = () =>
     this.setState(prevStateBad => ({ bad: prevStateBad.bad + 1 }));
 
+    // Calculate TOTAL quality of feedbacks
   countTotalFeedback = () => {
     let summ = 0;
     const arrayOfFeedbacks = Object.values(this.state);
@@ -25,11 +31,14 @@ export class App extends Component {
     }
     return summ;
   };
+
+  //===================================== Calculate Percentage of Positive Feedbacks
   countPositiveFeedbackPercentage = total => {
     const positivePercent = (this.state.good / total) * 100 || 0;
     return Math.round(positivePercent);
   };
 
+  //=========================================== Render JSX of feedback form
   render() {
     return (
       <div>
